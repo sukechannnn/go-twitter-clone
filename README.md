0abb50bc52cd082a2bf1a5d9d04bad17a468ee5d
sukechannnn <wonderfulworld0315@gmail.com>
2021-08-14 18:48:12 +0900

    Add create_users migration to sql-migrate

--- README.md
+++ README.md
@@ -6,4 +6,7 @@
 # Setup DB
 $ docker-compose up -d
 $ bin/setup_db
+
+# Install sql-migrate before execute below
+$ sql-migrate up
 ```
--- migrations/20210814174301-create_users.sql
+++ migrations/20210814174301-create_users.sql
@@ -1,5 +1,15 @@
 -- +migrate Up
-CREATE TABLE IF NOT EXISTS users (id int);
+create table if not exists users (
+  id uuid not null primary key,
+  email varchar(191) not null unique,
+  encrypted_password varchar(191) not null,
+  screen_id varchar(191),
+  screen_name varchar(191),
+  created_at timestamp not null,
+  updated_at timestamp not null
+);
+
+create index on users (email);
 
 -- +migrate Down
-DROP TABLE IF EXISTS users;
+drop table if exists users;
