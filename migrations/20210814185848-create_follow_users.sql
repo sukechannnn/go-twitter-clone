4b28ae22979f00b336f8a7ffc5e515a3a92de5ab
sukechannnn <wonderfulworld0315@gmail.com>
2021-08-14 19:14:21 +0900

    Add create_follow_users migration to sql-migrate

---
+++ migrations/20210814185848-create_follow_users.sql
@@ -0,0 +1,13 @@
+-- +migrate Up
+create table if not exists follow_users (
+  id uuid not null primary key,
+  user_id uuid not null,
+  follower_id uuid not null,
+  foreign key (user_id) references users(id),
+  foreign key (follower_id) references users(id)
+);
+
+create index on follow_users (user_id, follower_id);
+
+-- +migrate Down
+drop table if exists follow_users;
