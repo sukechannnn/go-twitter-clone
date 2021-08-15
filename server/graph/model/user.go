786b08c48faf85d6b38c3cf2c24c552be775214d
sukechannnn <wonderfulworld0315@gmail.com>
2021-08-15 17:29:58 +0900

    Refactoring: Move find & create user func to model.User

--- server/graph/model/user.go
+++ server/graph/model/user.go
@@ -1,6 +1,12 @@
 package model
 
-import "time"
+import (
+	"time"
+
+	"github.com/google/uuid"
+	"golang.org/x/crypto/bcrypt"
+	"gorm.io/gorm"
+)
 
 type NewUser struct {
 	Email      string `json:"email"`
@@ -17,4 +23,36 @@ type User struct {
 	EncryptedPassword string
 	CreatedAt         time.Time
 	UpdatedAt         time.Time
+}
+
+func CreateUser(db *gorm.DB, input NewUser) (string, error) {
+	id, _ := uuid.NewRandom()
+	hash, _ := bcrypt.GenerateFromPassword([]byte(input.Password), 10)
+
+	newUser := User{
+		ID:                id.String(),
+		Email:             input.Email,
+		EncryptedPassword: string(hash),
+		ScreenID:          input.ScreenID,
+		ScreenName:        input.ScreenName,
+		CreatedAt:         time.Now(),
+		UpdatedAt:         time.Now(),
+	}
+	if err := db.Create(&newUser).Error; err != nil {
+		return "", err
+	}
+	return id.String(), nil
+}
+
+func FindById(db *gorm.DB, id string) (*User, error) {
+	var user User
+	if err := db.Find(&user, "id = ?", id).Error; err != nil {
+		return nil, err
+	}
+	return &User{
+		ID:         user.ID,
+		Email:      user.Email,
+		ScreenID:   user.ScreenID,
+		ScreenName: user.ScreenName,
+	}, nil
 }
