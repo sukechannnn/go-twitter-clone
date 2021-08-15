9bb317a53158212bacb61dee89323ae084ba51b1
sukechannnn <wonderfulworld0315@gmail.com>
2021-08-15 11:24:37 +0900

    Add index & unique & notnull to screen_id
    - and add notnull to screen_name

--- db/migrations/20210814174301-create_users.sql
+++ db/migrations/20210814174301-create_users.sql
@@ -5,13 +5,14 @@ create table if not exists users (
   id uuid primary key not null,
   email varchar(191) not null unique,
   encrypted_password varchar(191) not null,
-  screen_id varchar(191),
-  screen_name varchar(191),
+  screen_id varchar(191) not null unique,
+  screen_name varchar(191) not null,
   created_at timestamp not null,
   updated_at timestamp not null
 );
 
 create index on users (email);
+create index on users (screen_id);
 
 -- +migrate Down
 drop table if exists users;
